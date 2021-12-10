ade39412662eebe4c4aed3597303969dc9b8b555
tharun <30896231@students.sd308.org>
2021-12-09 19:28:39 -0600

    Neoshootpt2

--- src/main/java/team/gif/robot/commands/RunNeoShooterVoltage.java
+++ src/main/java/team/gif/robot/commands/RunNeoShooterVoltage.java
@@ -5,9 +5,29 @@ import team.gif.robot.subsystems.NeoShooter;
 
 public class RunNeoShooterVoltage extends CommandBase {
 
-    // TSBaB: calling to set voltage to 2.5
+    public RunNeoShooterVoltage() {
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize(){
+    }
+
+    // T.SBaB: calling to set voltage to 2.5
     @Override
     public void execute() {
         NeoShooter.getInstance().setVoltage(2.5);
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    // T.SBab: Once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        NeoShooter.getInstance().setVoltage(0);
     }
 }
--- src/main/java/team/gif/robot/subsystems/NeoShooter.java
+++ src/main/java/team/gif/robot/subsystems/NeoShooter.java
@@ -1,10 +1,13 @@
 package team.gif.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import team.gif.robot.RobotMap;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.CANPIDController;
 
 public class NeoShooter extends SubsystemBase {
     private static NeoShooter instance = null;
-    private NeoShooter shooterMotor;
 
     // T.SBab Creating a getInstance for renNeoShooterVoltage.java
     public static NeoShooter getInstance() {
@@ -14,8 +17,23 @@ public class NeoShooter extends SubsystemBase {
         return instance;
     }
 
+    // T.SBab
+    private static final CANSparkMax shooterMotor = new CANSparkMax(RobotMap.MOTOR_SPARKMAX_ONE, CANSparkMaxLowLevel.MotorType.kBrushless);
+    private static final CANPIDController shooterPIDController = shooterMotor.getPIDController();
+
     // T.SBab public the setVoltage
     public void setVoltage(double voltage) {
         shooterMotor.setVoltage(voltage);
+    }
+
+    // T.SBab
+    private NeoShooter(){
+        super();
+        shooterMotor.restoreFactoryDefaults();
+        shooterMotor.enableVoltageCompensation(12);
+        //shooterMotor.setInverted(true); // C:false P:true
+        shooterMotor.setIdleMode(CANSparkMax.IdleMode.kCoast);
+        shooterMotor.setSmartCurrentLimit(40,40);
+        shooterMotor.burnFlash();
     }
 }
